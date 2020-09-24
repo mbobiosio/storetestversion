15edb15e78aa16efefa5d20de47d3defa225becf
Mbuodile Obiosio <cazewonder@gmail.com>
2020-09-24 20:11:36 +0100

    gitignore

--- include/dbconfig.php
+++ include/dbconfig.php
@@ -4,7 +4,7 @@ session_start();
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 try {
   //Username, Password and Database
-  $con = new mysqli("localhost", "username", "password", "Database");
+  $con = new mysqli("localhost", "admin", "planetx", "bananabox");
   $con->set_charset("utf8mb4");
 } catch(Exception $e) {
   error_log($e->getMessage());
